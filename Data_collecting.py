2b7de0fd1d958ffc8761fa3c7c865c09b5cc42ac
YGJW27 <sheart2v@outlook.com>
2021-06-21 16:04:33 +0800

    create

---
+++ Data_collecting.py
@@ -0,0 +1,31 @@
+# merge ADNIMERGE and CDR key variables
+import pandas as pd
+
+Root_Path = 'D:/Project/ADNI_Short/'
+ADNIMERGE = 'Study_Info/Data___Database/ADNIMERGE.csv'
+OUTPUT = 'D:/selected_data_ADNI.csv'
+
+merge_table = pd.read_csv(Root_Path + ADNIMERGE, low_memory=False)
+selected = merge_table.loc[:,[
+    'RID','PTID','VISCODE','M','DX_bl','DX','AGE','PTGENDER','PTEDUCAT','APOE4',
+    'CDRSB','ADAS11','ADAS13','MMSE','RAVLT_immediate','RAVLT_learning',
+    'RAVLT_forgetting','RAVLT_perc_forgetting','LDELTOTAL','DIGITSCOR','TRABSCOR','FAQ']]
+
+# delete error and duplicated rows
+selected = selected[selected.VISCODE != 'y1']
+selected = selected[selected.M != 3]
+selected = selected.drop_duplicates(subset=['RID', 'M'])
+
+id_iter = selected[selected.duplicated(subset=['RID'])==False].RID
+
+# Filter out subjects with examine less than 6 months
+thres = 36
+for id in id_iter:
+    long_flag = (selected[selected.RID == id].M >= thres).any()
+    if id == 6624:
+        if long_flag == False:
+            selected = selected[selected.RID != id]
+
+selected = selected.sort_values(by=['RID','M'])
+
+selected.to_csv(OUTPUT, index=False)
