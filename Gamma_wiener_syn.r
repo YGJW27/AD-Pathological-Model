68446737d939f8d76bec189b216f738429575e21
Jiawei Yang <38936596+YGJW27@users.noreply.github.com>
2022-06-16 19:53:02 +0800

    Add files via upload
    Given m and effects

--- Gamma_wiener_syn.r
+++ Gamma_wiener_syn.r
@@ -3,26 +3,25 @@ options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
 
+
 N <- 201
 T <- seq(0, N-1, by=1)
-k_pos <- 1
-k_neg <- 1
-lambda_pos <- 1
-lambda_neg <- 0.5
-omega_pos <- 1
-omega_neg <- -1
-omega_0 <- 0.3
+k_pos <- 5
+k_neg <- 5
+lambda_pos <- 3
+lambda_neg <- 3
+omega_pos <- 3
+omega_neg <- 1.5
+omega_0 <- -1
 effect_count_pos <- rep(0, N-1)
 effect_count_neg <- rep(0, N-1)
 kt_pos <- rep(0, N-1)
 kt_neg <- rep(0, N-1)
-effect_count_pos <- rep(0, N-1)
-effect_count_neg <- rep(0, N-1)
 effect_pos <- rep(0, N-1)
 effect_neg <- rep(0, N-1)
 mu <- rep(0, N-1)
 c_mu <- 0.5
-sigma <- 0.1
+sigma <- 0.01
 
 for (n in 1:(N-1)){
   kt_pos[n] <- k_pos * (T[n+1]-T[n])
@@ -31,7 +30,7 @@ for (n in 1:(N-1)){
   effect_count_neg[n] <- rpois(1, kt_neg[n])
   effect_pos[n] <- rgamma(1, effect_count_pos[n], lambda_pos)
   effect_neg[n] <- rgamma(1, effect_count_neg[n], lambda_neg)
-  mu[n] <- c_mu * tanh(omega_pos * effect_pos[n] + omega_neg * effect_neg[n] + omega_0)
+  mu[n] <- c_mu * tanh(omega_pos * effect_pos[n] - omega_neg * effect_neg[n] + omega_0) * (T[n+1]-T[n])
 }
 
 m <- rep(0, N)
@@ -50,7 +49,11 @@ for (n in 1:N){
 }
 
 
-model.file <- "D:/Documents/R/Gamma_wiener.stan"
-ad_dat <- list(N = N, T = T, y = y, pois_max = 20)
+model.file <- "C:/Users/shear/Documents/R/Gamma_wiener.stan"
+ad_dat <- list(N = N, T = T, y = y, pois_max = 20, effect_pos=effect_pos, effect_neg=effect_neg, m=m)
 fit <- stan(file = model.file, data = ad_dat, chains = 2, iter=5000)
 
+print(fit)
+plot(fit)
+traceplot(fit, pars=list("omega_pos","omega_neg"), include=TRUE)
+pairs(fit, pars = c("mu", "tau", "lp__"))
