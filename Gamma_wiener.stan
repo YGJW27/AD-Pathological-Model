d627c7e0103f98c5a599632f5a2c3c262ffcc36d
Jiawei Yang <38936596+YGJW27@users.noreply.github.com>
2022-07-09 00:39:54 +0800

    add physiological degradation process

--- Gamma_wiener.stan
+++ Gamma_wiener.stan
@@ -9,26 +9,32 @@ data {
 }
 
 transformed data {
-  
 }
 
 parameters {
   real omega_0;
   real<lower=0> omega_pos;
   real<lower=0> omega_neg;
+  real omega_d;
   real c_mu;
-  
+  real<lower=0.009,upper=0.011> sigma;
   real m[N_total];
-  real<lower=0> sigma;
   
+  real d[N_total];
+  real<lower=0> c_ga;
+  real<lower=0> beta;
+
   real h_0;
-  real h_1;
+  real h_d;
+  real h_m;
   real<lower=0> sigma_ep;
 }
 
 transformed parameters {
   real mu[N_total - N_sample];
   real dm[N_total - N_sample];
+  real alpha[N_total - N_sample];
+  real<lower=0> dd[N_total - N_sample];
   real mu_ob[N_total];
   
   {
@@ -38,34 +44,48 @@ transformed parameters {
       for (n in (N_pre+1):(N_pre+N_vec[idx]-1)){
         dm[n-idx+1] = m[n+1] - m[n];
         mu[n-idx+1] = c_mu * tanh(omega_pos * effect_pos[n-idx+1]
-            - omega_neg * effect_neg[n-idx+1] + omega_0) * (T[n+1] - T[n]);
+            - omega_neg * effect_neg[n-idx+1] + omega_d * d[n] + omega_0) * (T[n+1] - T[n]);
+        dd[n-idx+1] = d[n+1] - d[n];
+        alpha[n-idx+1] = c_ga * inv_logit(m[n]) * (T[n+1] - T[n]);
       }
       N_pre = N_pre + N_vec[idx];
     }
   }
   
   for (n in 1:N_total)
-    mu_ob[n] = h_0 + m[n] * h_1;
+    mu_ob[n] = h_0 + d[n] * h_d + m[n] * h_m;
 }
 
 model {
-  omega_pos ~ normal(3, 0.01);
-  omega_neg ~ normal(1.5, 0.01);
-  omega_0 ~ normal(-1, 0.01);
-  h_0 ~ normal(10, 0.01);
-  h_1 ~ normal(1, 0.01);
+  omega_pos ~ normal(0.1, 0.01);
+  omega_neg ~ normal(0.1, 0.01);
+  omega_d ~ normal(0.05, 0.001);
+  omega_0 ~ normal(0, 0.01);
+  c_mu ~ normal(3, 0.01);
+  c_ga ~ normal(10, 0.01);
+  beta ~ normal(10, 0.01);
+
+
+  h_0 ~ normal(30, 0.01);
+  h_d ~ normal(-1, 0.01);
+  h_m ~ normal(-0.5, 0.01);
+
   
   {
     int N_pre;
     N_pre = 0;
     for (idx in 1:N_sample){
       m[N_pre+1] ~ normal(0, 0.01);
+      d[N_pre+1] ~ normal(0, 0.01);
       N_pre = N_pre + N_vec[idx];
     }
   }
 
-  for (n in 1:(N_total - N_sample))
+  for (n in 1:(N_total - N_sample)){
     dm[n] ~ normal(mu[n], sigma);
+    dd[n] ~ gamma(alpha[n], beta);
+  }
+    
   
   for (n in 1:N_total)
     y[n] ~ normal(mu_ob[n], sigma_ep);
--- Gamma_wiener_syn.r
+++ Gamma_wiener_syn.r
@@ -1,15 +1,23 @@
 library("rstan")
+library("sigmoid")
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
 generate_data <- function(NS, N, k_pos, k_neg, lambda_pos, lambda_neg,
-                          omega_pos, omega_neg, omega_0, c_mu, sigma,
-                          h_0, h_1, sigma_ep){
+                          omega_pos, omega_neg, omega_d, omega_0, c_mu, sigma, 
+                          c_ga, beta, h_0, h_d, h_m, sigma_ep){
   N_vec <- c()
   T_total <- c()
   y_total <- c()
   effect_pos_total <- c()
   effect_neg_total <- c()
+  
+  mu_total <- c()
+  alpha_total <- c()
+  m_total <- c()
+  dm_total <- c()
+  d_total <- c()
+  dd_total <- c()
   for (i in 1:NS){
     T <- seq(0, N-1, by=1)
     effect_count_pos <- rep(0, N-1)
@@ -18,8 +26,7 @@ generate_data <- function(NS, N, k_pos, k_neg, lambda_pos, lambda_neg,
     kt_neg <- rep(0, N-1)
     effect_pos <- rep(0, N-1)
     effect_neg <- rep(0, N-1)
-    mu <- rep(0, N-1)
-    
+
     for (n in 1:(N-1)){
       kt_pos[n] <- k_pos * (T[n+1]-T[n])
       kt_neg[n] <- k_neg * (T[n+1]-T[n])
@@ -27,19 +34,29 @@ generate_data <- function(NS, N, k_pos, k_neg, lambda_pos, lambda_neg,
       effect_count_neg[n] <- rpois(1, kt_neg[n])
       effect_pos[n] <- rgamma(1, effect_count_pos[n], lambda_pos)
       effect_neg[n] <- rgamma(1, effect_count_neg[n], lambda_neg)
-      mu[n] <- c_mu * tanh(omega_pos * effect_pos[n] - omega_neg * effect_neg[n] + omega_0) * (T[n+1]-T[n])
     }
+    
+    mu <- rep(0, N-1)
+    alpha <- rep(0, N-1)
     
     m <- rep(0, N)
     dm <- rep(0, N-1)
+    d <- rep(0, N)
+    dd <- rep(0, N-1) 
+    
     for (n in 1:(N-1)){
+      mu[n] <- c_mu * tanh(omega_pos * effect_pos[n] - omega_neg * effect_neg[n]
+                           + omega_d * d[n] + omega_0) * (T[n+1]-T[n])
+      alpha[n] <- c_ga * sigmoid(m[n]) * (T[n+1]-T[n])
       dm[n] <- rnorm(1, mu[n], sigma)
       m[n+1] <- m[n] + dm[n]
+      dd[n] <- rgamma(1, alpha[n], beta)
+      d[n+1] <- d[n] + dd[n]
     }
     
     y <- rep(0, N)
     for (n in 1:N){
-      y[n] <- h_0 + h_1 * m[n] + rnorm(1, 0, sigma_ep)
+      y[n] <- h_0 + h_d * d[n] + h_m * m[n] + rnorm(1, 0, sigma_ep)
     }
     
     N_vec <- append(N_vec, N)
@@ -47,39 +64,59 @@ generate_data <- function(NS, N, k_pos, k_neg, lambda_pos, lambda_neg,
     y_total <- append(y_total, y)
     effect_pos_total <- append(effect_pos_total, effect_pos)
     effect_neg_total <- append(effect_neg_total, effect_neg)
+    
+    mu_total <- c(mu_total, mu)
+    alpha_total <- c(alpha_total, alpha)
+    m_total <- c(m_total, m)
+    dm_total <- c(dm_total, dm)
+    d_total <- c(d_total, d)
+    dd_total <- c(dd_total, dd)
   }
-  list(N_vec, T_total, y_total, effect_pos_total, effect_neg_total)
+  list(N_vec=N_vec, T_total=T_total, y_total=y_total, 
+       effect_pos_total=effect_pos_total, effect_neg_total=effect_neg_total,
+       mu_total=mu_total, alpha_total=alpha_total, m_total=m_total,
+       dm_total=dm_total, d_total=d_total, dd_total=dd_total)
 }
 
-N <- 101
+N <- 21
 k_pos <- 5
 k_neg <- 5
 lambda_pos <- 3
 lambda_neg <- 3
-omega_pos <- 3
-omega_neg <- 1.5
-omega_0 <- -1
-c_mu <- 0.5
+omega_pos <- 0.1
+omega_neg <- 0.1
+omega_d <- 0.05
+omega_0 <- 0
+c_mu <- 3
 sigma <- 0.01
+c_ga <- 10
+beta <- 10
 
-h_0 <- 10
-h_1 <- 1
-sigma_ep <- 0.01
+h_0 <- 30
+h_d <- -1
+h_m <- -0.5
+sigma_ep <- 0.5
 
 NS <- 2
+
 syn_data <- generate_data(NS, N, k_pos, k_neg, lambda_pos, lambda_neg,
-                          omega_pos, omega_neg, omega_0, c_mu, sigma,
-                          h_0, h_1, sigma_ep)
+                          omega_pos, omega_neg, omega_d, omega_0, c_mu, sigma,
+                          c_ga, beta, h_0, h_d, h_m, sigma_ep)
 
 
 model.file <- "D:/Documents/R/Gamma_wiener.stan"
-ad_dat <- list(N_sample = NS, N_vec = unlist(syn_data[1]), 
-               N_total = NROW(unlist(syn_data[2])), T = unlist(syn_data[2]), 
-               y = unlist(syn_data[3]), effect_pos=unlist(syn_data[4]), 
-               effect_neg=unlist(syn_data[5]))
+ad_dat <- list(N_sample = NS, N_vec = syn_data$N_vec, 
+               N_total = NROW(syn_data$T_total), T = syn_data$T_total, 
+               y = syn_data$y_total, effect_pos=syn_data$effect_pos_total, 
+               effect_neg=syn_data$effect_neg_total)
 
-fit <- stan(file = model.file, data = ad_dat, chains = 2, iter=5000)
+ad_init <- function() {
+  list(d = syn_data$d_total)
+}
 
+fit <- stan(file = model.file, data = ad_dat, init = ad_init, chains = 1, iter=5000)
+
+print(fit, pars=list("mu", "m", "mu_ob", "dm"), include=TRUE)
 print(fit, pars=list("mu", "m", "mu_ob", "dm"), include=FALSE)
 print(fit, pars=list("m"), include=TRUE)
 plot(fit)
